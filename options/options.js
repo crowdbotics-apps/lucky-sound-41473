233cb025da73cdf9d017ea7fb498a004030383f6
Crowdbotics <team@crowdbotics.com>
2023-06-14 12:56:01 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "lucky-sound-41473",
+  url: "https://lucky-sound-41473.botics.co",
+  api: "https://lucky-sound-41473.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
